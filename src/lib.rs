0a716a5bb396fb631885da849d95271630a02a54
dibek <dibekpoudyal@gmail.com>
2024-10-04 10:55:57 +0545

    pass server address on argument

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
 // greet will say hello to the person or world by default
 async fn greet(req: HttpRequest) -> impl Responder {
@@ -12,14 +13,14 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(address: &str) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(address)?
     .run();
 
     Ok(server)
